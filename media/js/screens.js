aff17801f823c0f69a27b20a1bbc1f00049d68d2
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2013-03-22 11:12:54 -0300

    Flag the current talk as '.current'

--- media/js/screens.js
+++ media/js/screens.js
@@ -4,6 +4,14 @@ jQuery(function($) {
   }
 
   $(document).on('page:change', function() {
+    var now = new Date();
+    var $table = now.getDay() == 5 ? $("table:first") : $("table:last");
+
+    $table.find("tr").removeClass('current').filter(function() {
+      var $tr = $(this);
+      var time = parseInt($tr.find(".time").text().replace(/\D+/g, ''));
+      return 100 * now.getHours() + now.getMinutes() > time;
+    }).last().addClass('current');
   });
 
   setInterval(reload, 30000);
